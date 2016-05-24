c71764b184d19354ff671e0836b44506f3b6f7bb
kevinmai <kevin.mai@vitalstorm.com>
2016-05-24 16:16:24 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8">
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div id="mobile_layout">
+			<header id="mobile_header">
+				<div class="container" id="mobile_header_wrapper">
+					<div id="mobile_home_logo">
+						<a href="/"><img alt="southern trust home services" src="images/southern-trust-mobile-logo.png" title="southern trust home services"></a>
+					</div>
+					<div>
+						<img alt="mobile navigation" id="mobile_nav" src="images/mobile-nav.png" title="mobile navigation">
+					</div>
+				</div>
+			</header>
+			<div id="services_icon">
+				<div class="container">
+					<div id="services_button">	
+						<div>
+							<a href=""><img alt="service 1" src="images/service-1.png"></a>
+						</div>
+						<div>
+							<a href=""><img alt="service 2" src="images/service-2.png"></a>
+						</div>
+						<div>
+							<a href=""><img alt="service 3" src="images/service-3.png"></a>
+						</div>
+						<div>
+							<a href=""><img alt="service 4" src="images/service-4.png"></a>
+						</div>
+					</div>
+					<div id="view_our_services">
+						<a href="#">View Our Services >></a>
+					</div>
+				</div>
+			</div>	
+			<div id="call_to_action_mobile">
+				<div class="container">
+					<div id="cta_mobile">
+						<div id="call_now_mobile">
+							<a href="tel:1234567890">
+								<img alt="call now" title="call now" src="images/call_now.png"><span>CALL NOW</span>
+							</a>
+						</div>
+						<div id="schedule_mobile">
+							<a href="#">
+								<img alt="schedule" title="schedule" src="images/schedule.png"><span>SCHEDULE</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="coupon_mobile">
+				<div class="container">
+					<img alt="coupon" title="coupon" src="images/coupon_mobile.png">
+				</div>
+			</div>
+			<div id="newsletter_form_mobile">
+				<div class="container">
+					<p>Sign up to receive even more great offers!</p>
+					<div id="mobile_form_wrapper">
+						<form id="form_body">
+							<input type="text" name="email" id="email_newsletter" placeholder="Enter your email address" value="">
+							<input type="submit" id="submit_newsletter" name="submitNewsletter" value="Submit">
+							<div class="clear"></div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div id="trust_section">
+				<div class="container">
+					<div id="trust_rotator">
+						<img alt="trust 1" title="trust 1" id="trust_1" src="images/family-owned.png">
+						<div id="indicators">
+							<ul>
+								<li></li>
+								<li></li>
+								<li></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="testimonials_mobile">
+				<div class="container">
+					<div id="reviews">
+						<p class="title">We’re really good, ask our customers!</p>
+						<img alt="stars" id="stars" title="stars" src="images/stars.png">
+						<p class="customer_review">“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius elementum metus. In accumsan orci a tristique auctor. Duis eget maximus erat.”</p>
+						<p class="customer_name">- John Smith</p>
+					</div>
+				</div>
+			</div>
+			<footer id="mobile_footer">
+			<div class="container">
+				<div id="social_media_button">	
+					<div>
+						<a href=""><img alt="facebook" src="images/facebook.png"></a>
+					</div>
+					<div>
+						<a href=""><img alt="twitter" src="images/twitter.png"></a>
+					</div>
+					<div>
+						<a href=""><img alt="linkedin" src="images/linkedin.png"></a>
+					</div>
+					<div>
+						<a href=""><img alt="google plus" src="images/googleplus.png"></a>
+					</div>
+				</div>
+				<div id="mobile_address">
+					<p>Southern Trust Home Services</p>
+					<p>5205 Starkey Rd. Unit B</p>
+					<p>Roanoke, VA 24018</p>
+				</div>
+				<div id="mobile_copyright">
+					<p>© 2016 All Rights Reserved. Southern Trust Home Services</p>
+					<p>Marketing for home services by Vitalstorm.</p>
+				</div>
+			</div>
+		</footer>
+		</div>
+		<div id="desktop_layout">
+			<div class="container">
+				<h1>I am desktop</h1>
+			</div>
+		</div>
+
+	</body>
+</html>
